f48df7290c7bab1c81e0142f02c11626e044f2db
marziehShooshtarii <shooshtarimarzieh83@gmail.com>
2025-07-21 00:53:49 +0330

    MillenniumFalcon spaceship created

---
+++ MillenniumFalcon.hpp
@@ -0,0 +1,12 @@
+#ifndef MILLENNIUMFALCON.HPP
+#define MILLENNIUMFALCON .HPP
+#include "Spaceship.hpp"
+
+class MillenniumFalcon : public Spaceship
+{
+    MillenniumFalcon(int dis, int radar, int distruct, int controlDictance, std::string type);
+
+public:
+private:
+};
+#endif
