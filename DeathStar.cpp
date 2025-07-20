04dd4d23aee8fce8531e045aa48a36e0b3de3bfe
marziehShooshtarii <shooshtarimarzieh83@gmail.com>
2025-07-21 00:53:05 +0330

    deathStar spaceship created

---
+++ DeathStar.cpp
@@ -0,0 +1,2 @@
+#include "DeathStar.hpp"
+DeathStar::DeathStar(int dis, int radar, int distruct, int controlDictance, std::string type) : Spaceship(INT_MAX, 2, 1488, 500, "D1") {}
---
+++ DeathStar.hpp
@@ -0,0 +1,12 @@
+#ifndef DEATHSTAR.HPP
+#define DEATHSTAR.HPP
+#include "Spaceship.hpp"
+
+class DeathStar : public Spaceship
+{
+    DeathStar(int dis, int radar, int distruct, int controlDictance, std::string type);
+
+public:
+private:
+};
+#endif
