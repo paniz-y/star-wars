a3415fb4194290f4b163e7d0007f124e29303eba
paniz-y <paniz84yaghoobzadeh@gmail.com>
2025-07-20 23:21:51 +0330

    CivilCity class created

---
+++ CivilCity.hpp
@@ -0,0 +1,15 @@
+#ifndef CIVILCITY.HPP
+#define CIVILCITY .HPP
+#include "City.hpp"
+#include "Spaceship.hpp"
+#include <vector>
+class CivilCity : public City
+{
+public:
+private:
+
+    
+    
+};
+
+#endif
