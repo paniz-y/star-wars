f1c49f3f3b29dd2bb134a9799b294ff51c4be289
marziehShooshtarii <shooshtarimarzieh83@gmail.com>
2025-07-21 00:54:09 +0330

    razor crest spaceship created

---
+++ RazorCrest.cpp
@@ -0,0 +1,3 @@
+#include "RazorCrest.hpp"
+
+RazorCrest::RazorCrest(int dis, int radar, int distruct, int controlDictance, std::string type) : Spaceship(3000, 2, 110, 700, "C1") {}
---
+++ RazorCrest.hpp
@@ -0,0 +1,12 @@
+#ifndef RAZORCREST.HPP
+#define RAZORCREST .HPP
+#include "Spaceship.hpp"
+#include <climits>
+class RazorCrest : public Spaceship
+{
+    RazorCrest(int dis, int radar, int distruct, int controlDictance, std::string type);
+
+public:
+private:
+};
+#endif
