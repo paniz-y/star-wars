ad4b9cc589ee4abd44eacc842f3592d1f71c3552
marziehShooshtarii <shooshtarimarzieh83@gmail.com>
2025-07-21 00:52:36 +0330

    Awing spaceship created

---
+++ Awing.hpp
@@ -0,0 +1,12 @@
+#ifndef AWING.HPP
+#define AWING .HPP
+#include "Spaceship.hpp"
+
+class Awing : public Spaceship
+{
+    Awing(int dis, int radar, int distruct, int controlDictance, std::string type);
+
+public:
+private:
+};
+#endif
