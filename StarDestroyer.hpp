ce3c687afcf77a298aef671b9f84498d702646cf
marziehShooshtarii <shooshtarimarzieh83@gmail.com>
2025-07-21 00:54:53 +0330

    star destroyer spaceship created

---
+++ StarDestroyer.hpp
@@ -0,0 +1,12 @@
+#ifndef STARDESTORY.HPP
+#define STARDESTORY .HPP
+#include "Spaceship.hpp"
+
+class StarDestroyer : public Spaceship
+{
+    StarDestroyer(int dis, int radar, int distruct, int controlDictance, std::string type);
+
+public:
+private:
+};
+#endif
