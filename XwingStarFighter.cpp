047f5be7639897616d7a5d785af230e59f1d2cac
marziehShooshtarii <shooshtarimarzieh83@gmail.com>
2025-07-21 00:55:22 +0330

    XwingStar fighter spaceship created

---
+++ XwingStarFighter.cpp
@@ -0,0 +1,3 @@
+#include "XwingStarFighter.hpp"
+
+XwingStarFighter::XwingStarFighter(int dis, int radar, int distruct, int controlDictance, std::string type) : Spaceship(2500, 2, 130, 500, "A2") {}
---
+++ XwingStarFighter.hpp
@@ -0,0 +1,12 @@
+#ifndef XWINGSTARFIGHTER.HPP
+#define XWINGSTARFIGHTER .HPP
+#include "Spaceship.hpp"
+
+class XwingStarFighter : public Spaceship
+{
+    XwingStarFighter(int dis, int radar, int distruct, int controlDictance, std::string type);
+
+public:
+private:
+};
+#endif
