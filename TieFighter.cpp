e2bc720057fb983ecb1fa181ad6b9e4d70ac1d94
marziehShooshtarii <shooshtarimarzieh83@gmail.com>
2025-07-21 00:54:27 +0330

    tie fighter spaceship created

---
+++ TieFighter.cpp
@@ -0,0 +1,3 @@
+#include "TieFighter.hpp"
+
+TieFighter::TieFighter(int dis, int radar, int distruct, int controlDictance, std::string type) : Spaceship(2500, 4, 50, 500, "A3") {}
---
+++ TieFighter.hpp
@@ -0,0 +1,12 @@
+#ifndef TIEFIGHTER.HPP
+#define TIEFIGHTER .HPP
+#include "Spaceship.hpp"
+
+class TieFighter : public Spaceship
+{
+    TieFighter(int dis, int radar, int distruct, int controlDictance, std::string type);
+
+public:
+private:
+};
+#endif
