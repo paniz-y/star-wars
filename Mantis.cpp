c4bae4f9334b544ec7cd14aae406bcd11c97a8e2
marziehShooshtarii <shooshtarimarzieh83@gmail.com>
2025-07-21 00:53:23 +0330

    mantis spaceship created

---
+++ Mantis.cpp
@@ -0,0 +1,3 @@
+#include "Mantis.hpp"
+
+Mantis::Mantis(int dis, int radar, int distruct, int controlDictance, std::string type) : Spaceship(5000, 0, 300, 500, "B2") {}
---
+++ Mantis.hpp
@@ -0,0 +1,12 @@
+#ifndef MANTIS.HPP
+#define MANTIS.HPP
+#include "Spaceship.hpp"
+
+class Mantis : public Spaceship
+{
+    Mantis(int dis, int radar, int distruct, int controlDictance, std::string type);
+
+public:
+private:
+};
+#endif
