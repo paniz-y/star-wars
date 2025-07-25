15350f89dab96e8b42db220e8f2b3ef7bbe063e9
marziehShooshtarii <shooshtarimarzieh83@gmail.com>
2025-07-25 23:31:05 +0330

    constructor added for the class

--- CivilCity.hpp
+++ CivilCity.hpp
@@ -1,11 +1,12 @@
-#ifndef CIVILCITY.HPP
-#define CIVILCITY .HPP
+#ifndef CIVILCITY_HPP
+#define CIVILCITY_HPP
 #include "City.hpp"
 #include "Spaceship.hpp"
 #include <vector>
 class CivilCity : public City
 {
 public:
+CivilCity(std::pair<int , int> , bool);
 private:
 
     
