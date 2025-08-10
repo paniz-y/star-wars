8a78ce5f138aaa7b849cf19779da5ea3807b3548
paniz-y <paniz84yaghoobzadeh@gmail.com>
2025-08-10 20:02:58 +0330

    unnecessary comments deleted

--- Defense.hpp
+++ Defense.hpp
@@ -8,34 +8,10 @@ public:
     void setRatio(int ratio);
     int getRatio() const;
     void defend();
-    // bool operator==(const Defense otherDefense) const
-    // {
-    //     return (coordinatesForDefense == otherDefense.coordinatesForDefense) && (defenseRatio == otherDefense.defenseRatio);
-    // }
-    // friend struct std::hash<Defense>;
-    // friend class EnemyCity;
+
 private:
     std::pair<int, int> coordinatesForDefense;
     int defenseRatio;
 };
-// namespace std
-// {
-//     template <>
-//     struct hash<Defense>
-//     {
-//         size_t operator()(const Defense &defense) const
-//         {
-//             auto tuple = std::tie(defense.coordinatesForDefense.first, defense.coordinatesForDefense.second);
-//             return hash<decltype(tuple)>()(tuple);
-//         }
-//     };
-// }
-// struct HashForPair {
-//     template <typename T1, typename T2>
-//     std::size_t operator()(const std::pair<T1, T2>& p) const {
-//         std::size_t h1 = std::hash<T1>{}(p.first);
-//         std::size_t h2 = std::hash<T2>{}(p.second);
-//         return h1 ^ (h2 << 1);
-//     }
-// };
+
 #endif
