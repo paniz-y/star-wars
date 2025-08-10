e688672ee26871d695e2f2ae42c6ebe988ed0f90
paniz-y <paniz84yaghoobzadeh@gmail.com>
2025-08-10 17:51:37 +0330

    unnecessary comments in map deleted

--- Map.cpp
+++ Map.cpp
@@ -3,19 +3,12 @@ std::vector<std::pair<std::shared_ptr<City>, double>> Map::getNeighbors(std::sha
 {
     return locationsGraph[city];
 }
+
 void Map::addEdge(std::shared_ptr<City> firstVertex, std::shared_ptr<City> secondVertex, double weight)
 {
     locationsGraph[firstVertex].emplace_back(secondVertex, weight);
     locationsGraph[secondVertex].emplace_back(firstVertex, weight);
 }
-
-// void Map::graphBaseCitys(std::vector<std::pair<int, int>> baseCityCoodinates, int baseCityNumOfSpys, std::vector<Spaceship> baseCitySpaceships)
-// {
-//     for (int i = 0; i < baseCityCoodinates.size(); i++)
-//     {
-//         std::shared_ptr<BaseCity> base = std::make_shared<BaseCity>(baseCityCoodinates[i], baseCityNumOfSpys, baseCitySpaceships);
-//     }
-// }
 
 int Map::calculateWeightForSpys(std::shared_ptr<City> firstVertex, std::shared_ptr<City> secondVertex)
 {
@@ -29,37 +22,6 @@ int Map::calculateWeightForSpys(std::shared_ptr<City> firstVertex, std::shared_p
     }
     return 0;
 }
-// int Map::calculateWeightForDefenses(std::shared_ptr<City> firstVertex, std::shared_ptr<City> secondVertex)
-// {
-//     // if(firstVertex->getHasDefence() || secondVertex->getHasDefence())
-//     // {
-//     //     return maxSize * 2;
-//     // }
-//     if (std::shared_ptr<EnemyCity> enemy = std::dynamic_pointer_cast<EnemyCity>(firstVertex))
-//     {
-//         return maxSize * 2;
-//     }
-//     if (std::shared_ptr<EnemyCity> enemy = std::dynamic_pointer_cast<EnemyCity>(secondVertex))
-//     {
-//         return maxSize * 2;
-//     }
-//     return 0;
-// }
-
-// void Map::removeDefense(std::shared_ptr<City> firstVertex)
-// {
-//     for (auto neighbor : locationsGraph[firstVertex])
-//     {
-
-//        neighbor.second.second = 0;
-//     }
-// }
-
-// std::pair<double, double> Map::pairWeightForSpysAndWeightForDefenses(double weightForSpys, double weightForDefenses)
-// {
-//     std::make_pair()
-//     return std::pair<double, double>();
-// }
 
 void Map::graphMap(std::vector<std::shared_ptr<City>> cities)
 {
@@ -72,16 +34,6 @@ void Map::graphMap(std::vector<std::shared_ptr<City>> cities)
         }
     }
 }
-// void Map::graphMapWithDefenses(std::vector<std::shared_ptr<City>> cities)
-// {
-//     for (int i = 0; i < cities.size() - 1; i++)
-//     {
-//         for (int j = i + 1; j < cities.size(); j++)
-//         {
-//             addEdge(cities[i], cities[j], calculateWeightForDefenses(cities[i], cities[j]));
-//         }
-//     }
-// }
 
 int Map::getMaxSize()
 {
--- Map.hpp
+++ Map.hpp
@@ -13,18 +13,10 @@ class Map
 public:
     std::vector<std::pair<std::shared_ptr<City>, double>> getNeighbors(std::shared_ptr<City> city);
     void addEdge(std::shared_ptr<City> firstVertex, std::shared_ptr<City> secondVertex, double weight);
-    // void graphBaseCitys(std::vector<std::pair<int, int>> baseCityCoodinates, int baseCityNumOfSpys, std::vector<Spaceship> baseCitySpaceships);
-    // std::vector<std::pair<std::shared_ptr<City>, double>> getNeighbors(std::shared_ptr<City> city);
-
     void graphMap(std::vector<std::shared_ptr<City>> cities);
-    //void graphMapWithDefenses(std::vector<std::shared_ptr<City>> cities);
-
     int getMaxSize();
     void setMaxSize(int sizeOfMap);
-    int calculateWeightForSpys(std::shared_ptr<City> firstVertex, std::shared_ptr<City> secondVertex);     // calculates edge weights based on spys along the way
-    //int calculateWeightForDefenses(std::shared_ptr<City> firstVertex, std::shared_ptr<City> secondVertex); // calculates edge weights based on defenses along the way
-    //std::pair<double , double> pairWeightForSpysAndWeightForDefenses(double weightForSpys, double weightForDefenses);
-    //void removeDefense(std::shared_ptr<City> firstVertex);
+    int calculateWeightForSpys(std::shared_ptr<City> firstVertex, std::shared_ptr<City> secondVertex); // calculates edge weights based on spys along the way
 private:
     std::unordered_map<std::shared_ptr<City>, std::vector<std::pair<std::shared_ptr<City>, double>>> locationsGraph;
     int maxSize;
