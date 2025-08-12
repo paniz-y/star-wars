bcf0128da42f7baa32d65e9f85f0e5be6ce647c7
marziehShooshtarii <shooshtarimarzieh83@gmail.com>
2025-08-12 03:42:09 +0330

    starDestroyer name added

---  StarDestroyer.cpp
+++  StarDestroyer.cpp
@@ -1,3 +1,6 @@
 #include "StarDestroyer.hpp"
 
-StarDestroyer::StarDestroyer() : Spaceship(2900, 1, 10, 900, "C") {}
+StarDestroyer::StarDestroyer() : Spaceship(2900, 1, 10, 900, "C")
+{
+    setName("StarDestroyer");
+}
