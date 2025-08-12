0809d07933cdcc70e40d6d14c6bd06439a65948b
marziehShooshtarii <shooshtarimarzieh83@gmail.com>
2025-08-12 03:41:38 +0330

    razorcrest name added

--- RazorCrest.cpp
+++ RazorCrest.cpp
@@ -1,3 +1,6 @@
 #include "RazorCrest.hpp"
 
-RazorCrest::RazorCrest() : Spaceship(3000, 2, 110, 700, "C1") {}
+RazorCrest::RazorCrest() : Spaceship(3000, 2, 110, 700, "C1")
+{
+    setName("RazorCrest");
+}
