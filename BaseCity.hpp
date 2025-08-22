9322292fbc540d4833a459bdaa2fb15afb2ccb57
marziehShooshtarii <shooshtarimarzieh83@gmail.com>
2025-08-23 00:50:24 +0330

    virtual function overrided

--- BaseCity.hpp
+++ BaseCity.hpp
@@ -11,7 +11,7 @@ public:
     BaseCity(std::pair<int, int> coords, bool spy);
     void setListOfSpaceships(std::vector<std::shared_ptr<Spaceship>> spaceships);
     std::vector<std::shared_ptr<Spaceship>> getListOfSpaceships();
-    int getCapacity() const;
+    int getCapacity() const override;
     void setCapacity(int capacity);
 
 private:
